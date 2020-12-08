f05f34d3687258f64aed0478b614ec1f4806a4e7
haydenchung03 <hayden.chung21@ycdsbk12.ca>
2020-12-08 14:32:39 +0000

    Set up actions and squirrels.java

---
+++ src/Actions.java
@@ -0,0 +1,14 @@
+package SquirrelDemo;
+
+public class Actions{
+
+  private boolean blnSquirrel;
+  private String strRun;
+  private String strWalk;
+  private String strEat;
+  private String strClimb;
+
+  public Actions(String strNewRun, String strNewWalk, String strNewEat, String strNewClimb){
+    this.blnSquirrel = true;
+  }
+}
---
+++ src/Squirrel.java
@@ -0,0 +1,9 @@
+package SquirrelsDemo;
+
+import SquirrelsDemo.Actions;
+
+public class Squirrel{
+  public static void main(String[] Args){
+    
+  }
+}
